0b64c0c13cb79b10ccf6281a204109dbf1762dd2
DESKTOP-5JIE8GR\provi <leduchoang011@gmail.com>
2021-09-06 12:43:06 -0400

    Initial commit

---
+++ src/tetris/Board.java
@@ -0,0 +1,274 @@
+package tetris;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.Random;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel implements KeyListener, MouseListener, MouseMotionListener {
+
+   	//board dimensions (the playing area)
+    private final int boardHeight = 20, boardWidth = 10;
+
+	// block size
+    public static final int blockSize = 30;
+
+	// field
+    private Color[][] board = new Color[boardHeight][boardWidth];
+
+	// array with all the possible shapes
+    private Shape[] shapes = new Shape[7];
+
+	// currentShape
+    private static Shape currentShape, nextShape;
+
+	// game loop
+    private Timer looper;
+
+    private int FPS = 60;
+
+    private int delay = 1000 / FPS;
+
+	// mouse events variables
+    private int mouseX, mouseY;
+
+    private boolean leftClick = false;
+
+    private Color[] colors = {Color.decode("#ed1c24"), Color.decode("#ff7f27"), Color.decode("#fff200"), 
+        Color.decode("#22b14c"), Color.decode("#00a2e8"), Color.decode("#a349a4"), Color.decode("#3f48cc")};
+    private Random random = new Random();
+	// buttons press lapse
+    private Timer buttonLapse = new Timer(300, new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            buttonLapse.stop();
+        }
+    });
+
+	// score
+    private int score = 0;
+
+    public Board() {
+
+
+        mouseX = 0;
+        mouseY = 0;
+
+
+		// create game looper
+        looper = new Timer(delay, new GameLooper());
+
+		// create shapes
+        shapes[0] = new Shape(new int[][]{
+            {1, 1, 1, 1} // I shape;
+        }, this, colors[0]);
+
+        shapes[1] = new Shape(new int[][]{
+            {1, 1, 1},
+            {0, 1, 0}, // T shape;
+        }, this, colors[1]);
+
+        shapes[2] = new Shape(new int[][]{
+            {1, 1, 1},
+            {1, 0, 0}, // L shape;
+        }, this, colors[2]);
+
+        shapes[3] = new Shape(new int[][]{
+            {1, 1, 1},
+            {0, 0, 1}, // J shape;
+        }, this, colors[3]);
+
+        shapes[4] = new Shape(new int[][]{
+            {0, 1, 1},
+            {1, 1, 0}, // S shape;
+        }, this, colors[4]);
+
+        shapes[5] = new Shape(new int[][]{
+            {1, 1, 0},
+            {0, 1, 1}, // Z shape;
+        }, this, colors[5]);
+
+        shapes[6] = new Shape(new int[][]{
+            {1, 1},
+            {1, 1}, // O shape;
+        }, this, colors[6]);
+
+    }
+
+    private void update() {
+
+        currentShape.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+
+                if (board[row][col] != null) {
+                    g.setColor(board[row][col]);
+                    g.fillRect(col * blockSize, row * blockSize, blockSize, blockSize);
+                }
+
+            }
+        }
+        g.setColor(nextShape.getColor());
+        for (int row = 0; row < nextShape.getCoords().length; row++) {
+            for (int col = 0; col < nextShape.getCoords()[0].length; col++) {
+                if (nextShape.getCoords()[row][col] != 0) {
+                    g.fillRect(col * 30 + 320, row * 30 + 50, Board.blockSize, Board.blockSize);
+                }
+            }
+        }
+        currentShape.render(g);
+
+        g.setColor(Color.WHITE);
+
+        g.setFont(new Font("Georgia", Font.BOLD, 20));
+
+        g.drawString("SCORE", WindowGame.WIDTH - 125, WindowGame.HEIGHT / 2);
+        g.drawString(score + "", WindowGame.WIDTH - 125, WindowGame.HEIGHT / 2 + 30);
+
+        g.setColor(Color.WHITE);
+
+        for (int i = 0; i <= boardHeight; i++) {
+            g.drawLine(0, i * blockSize, boardWidth * blockSize, i * blockSize);
+        }
+        for (int j = 0; j <= boardWidth; j++) {
+            g.drawLine(j * blockSize, 0, j * blockSize, boardHeight * 30);
+        }
+    }
+
+    public void setNextShape() {
+        int index = random.nextInt(shapes.length);
+        int colorIndex = random.nextInt(colors.length);
+        nextShape = new Shape(shapes[index].getCoords(), this, colors[colorIndex]);
+    }
+
+    public void setCurrentShape() {
+        currentShape = nextShape;
+        setNextShape();
+
+
+    }
+
+    public Color[][] getBoard() {
+        return board;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            currentShape.setDeltaX(1);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            currentShape.setDeltaX(-1);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            currentShape.speedUp();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            currentShape.speedDown();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void startGame() {
+        stopGame();
+        setNextShape();
+        setCurrentShape();
+        looper.start();
+
+    }
+
+    public void stopGame() {
+        score = 0;
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = null;
+            }
+        }
+        looper.stop();
+    }
+
+    class GameLooper implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            update();
+            repaint();
+        }
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            leftClick = true;
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            leftClick = false;
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void addScore() {
+        score++;
+    }
+
+}
---
+++ src/tetris/WindowGame.java
@@ -0,0 +1,41 @@
+package tetris;
+
+import javax.swing.JFrame;
+
+public class WindowGame {
+    public static final int WIDTH = 445, HEIGHT = 629;
+
+    private Board board;
+
+    private JFrame window;
+
+    public WindowGame() {
+
+        window = new JFrame("Tetris");
+        window.setSize(WIDTH, HEIGHT);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+
+        board = new Board();
+
+
+        window.addKeyListener(board);
+
+
+
+
+        window.setVisible(true);
+        window.addMouseMotionListener(board);
+        window.addMouseListener(board);
+        window.add(board);
+        board.startGame();
+        window.revalidate();
+    }
+
+
+    public static void main(String[] args) {
+        new WindowGame();
+    }
+
+}
